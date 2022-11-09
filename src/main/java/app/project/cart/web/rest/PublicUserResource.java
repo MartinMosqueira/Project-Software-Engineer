b60e69ebf950374a8230fec9f49aac357b3936bb
MartinMosqueira <tinchomosqueira@gmail.com>
2022-11-09 08:33:28 -0300

    Initial project structure

--- src/main/java/app/project/cart/web/rest/PublicUserResource.java
+++ src/main/java/app/project/cart/web/rest/PublicUserResource.java
@@ -38,7 +38,7 @@ public class PublicUserResource {
      * @param pageable the pagination information.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all users.
      */
-    @GetMapping("/users")
+    @GetMapping("/users/public")
     public ResponseEntity<List<UserDTO>> getAllPublicUsers(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
         log.debug("REST request to get all public User names");
         if (!onlyContainsAllowedProperties(pageable)) {
